b01a1c087d0da490003d22811339375b9d9b7fcb
jyothipasumarthi <jyothi1250@sasi.ac.in>
2022-11-22 20:42:00 +0530

    commit

---
+++ funcn.py
@@ -0,0 +1,9 @@
+Here the meaning of the keywords are:
+
+def: this tells
+list of parameters: these are the list of parameters used by our function. 
+Parameters are values passed to the function. If we don’t want to pass any values, we can simply keep empty parentheses (). 
+
+The function Body: the list of instructions or statements that we want this method to perform
+
+Return statement: either returns a value or nothing at all. It is the last statement executed in a function
