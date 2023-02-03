59e576fc758b1a2a24f7c5566264e3f7acb5bf7b
jyothipasumarthi <jyothi1250@sasi.ac.in>
2023-02-03 07:13:13 +0530

    commit

---
+++ lists.py
@@ -0,0 +1,15 @@
+Method	        Description
+append()	    Add Single Element to The List
+extend()	    Add Elements of a List to Another List
+insert()	    Inserts Element to The List
+remove()	    Removes Element from the List
+index()	        returns smallest index of element in list
+count()	        returns occurrences of element in a list
+pop()	        Removes Element at Given Index
+reverse()	    Reverses a List
+sort()	        sorts elements of a list
+clear()	        Removes all Items from the List
+len()	        Returns Length of an Object
+max()	        returns largest element
+min()	        returns smallest element
+sum()	        Add items of an Iterable
